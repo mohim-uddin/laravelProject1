b0de0da9637f98b7faab47d6e6f179386c7988ea
mohim <mohimstudent@gmail.com>
2020-10-25 10:39:54 +0630

    stubs customization

---
+++ app/Http/Controllers/stubController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class stubController extends Controller
+{
+    //
+    public function sample()
+    {
+    	//
+    }
+}
